ea7112f9db3518077118177ec078d1a5ab1f158e
AnshSinghal-CyberUltron <ansh@zeroshield.ai>
2026-04-07 18:42:55 +0530

    test: second one-word change for incremental cache test (Copyright → Licensed)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2026 Bjoern Kimminich & the OWASP Juice Shop
+ * Licensed (c) 2014-2026 Bjoern Kimminich & the OWASP Juice Shop
  * maintainers.
  * SPDX-License-Identifier: MIT
  */
@@ -14,78 +14,25 @@ module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
-    replace_json: {
-      manifest: {
-        src: 'package.json',
-        changes: {
-          'engines.node': (node || '<%= pkg.engines.node %>'),
-          os: (os ? [os] : '<%= pkg.os %>'),
-          cpu: (platform ? [platform] : '<%= pkg.cpu %>')
-        }
-      }
-    },
-
-    compress: {
-      pckg: {
-        options: {
-          mode: os === 'linux' ? 'tgz' : 'zip',
-          archive: 'dist/<%= pkg.name %>-<%= pkg.version %>' + (node ? ('_node' + node) : '') + (os ? ('_' + os) : '') + (platform ? ('_' + platform) : '') + (os === 'linux' ? '.tgz' : '.zip')
-        },
-        files: [
-          {
-            src: [
-              '.well-known/**',
-              'LICENSE',
-              '*.md',
-              'package.json',
-              'ctf.key',
-              'swagger.yml',
-              'server.ts',
-              'config.schema.yml',
-              'build/**',
-              '!build/reports/**',
-              'bom.json',
-              'bom.xml',
-              'config/*.yml',
-              'data/*.ts',
-              'data/static/**',
-              'data/chatbot/.gitkeep',
-              'encryptionkeys/**',
-              'frontend/dist/frontend/**',
-              'frontend/dist/bom/**',
-              'frontend/src/**/*.ts',
-              'ftp/**',
-              'i18n/.gitkeep',
-              'lib/**',
-              'models/*.ts',
-              'node_modules/**',
-              'routes/*.ts',
-              'uploads/complaints/.gitkeep',
-              'views/**'
-            ],
-            dest: 'juice-shop_<%= pkg.version %>/'
-          }
-        ]
+    replace: {
+      version: {
+        src: ['./package.json'],
+        overwrite: true,
+        replacements: [{
+          from: /("version": )"([0-9]+\.[0-9]+\.[0-9]+)"/,
+          to: '$1"<%= pkg.version %>"'
+        }]
       }
     }
   })
 
-  grunt.registerTask('checksum', 'Create .md5 checksum files', function () {
-    const fs = require('node:fs')
-    const crypto = require('node:crypto')
-    fs.readdirSync('dist/').forEach(file => {
-      const buffer = fs.readFileSync('dist/' + file)
-      const md5 = crypto.createHash('md5')
-      md5.update(buffer)
-      const md5Hash = md5.digest('hex')
-      const md5FileName = 'dist/' + file + '.md5'
-      grunt.file.write(md5FileName, md5Hash)
-      grunt.log.write(`Checksum ${md5Hash} written to file ${md5FileName}.`).verbose.write('...').ok()
-      grunt.log.writeln()
-    })
+  grunt.registerTask('checkIfPackaged', function () {
+    if (!os || !platform || !node) {
+      grunt.log.warn('One or more of the mandatory options --os, --platform and --node are missing.')
+      return false
+    }
   })
 
-  grunt.loadNpmTasks('grunt-replace-json')
-  grunt.loadNpmTasks('grunt-contrib-compress')
-  grunt.registerTask('package', ['replace_json:manifest', 'compress:pckg', 'checksum'])
+  grunt.loadNpmTasks('grunt-text-replace')
+  grunt.registerTask('default', ['checkIfPackaged'])
 }
